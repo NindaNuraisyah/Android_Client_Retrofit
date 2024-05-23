ffec04e229ac422261d6aa527939722d2b131c65
Ninda_Nuraisyah <nindanuraisyah0903@gmail.com>
2024-05-23 23:01:27 +0700

    Praktikum

---
+++ app/src/main/java/com/ninda/android_client/User.java
@@ -0,0 +1,24 @@
+package com.ninda.android_client;
+
+public class User {
+    private int id;
+    private String name;
+    private String email;
+
+    public User(String name, String email) {
+        this.name = name;
+        this.email = email;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+}
